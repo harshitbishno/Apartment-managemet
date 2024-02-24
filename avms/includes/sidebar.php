da20e46c1db43b01241e12ab9a40c49623168328
harshitbishno <vishnoiharshit34@gmail.com>
2024-02-24 16:50:10 +0530

    Add new file

---
+++ avms/includes/sidebar.php
@@ -0,0 +1,56 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+        
+                   <h1>AMS</h1>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: purple">
+                                <i class="fas fa-tachometer-alt"></i>Welcome</a>
+                        </li>
+     <li><a href="category.php"  style="color: purple">
+                            <i class="fa fa-files-o"></i>Class</a>
+                        </li>
+
+
+
+     <li><a href="visitors-form.php"  style="color: purple">
+                            <i class="fa fa-user"></i>New Entry</a>
+                        </li>
+   <li>
+                            <a href="manage-newvisitors.php"  style="color: purple">
+                                <i class="fa fa-users"></i>Manage Old</a>
+                        </li>
+
+<li class="has-sub">
+<a class="js-arrow open" href="#">
+<i class="fas fa-copy"></i>Access pass</a>
+<ul class="list-unstyled navbar__sub-list js-sub-list">
+<li>
+<a href="create-pass.php">Create</a>
+</li>
+<li>
+<a href="manage-passes.php">Manage</a>
+</li>
+</ul>
+</li>
+
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: purple">
+                                <i class="fas fa-copy"></i>Vistors Log</a>
+                        </li>  
+
+                      <li>
+                            <a href="bwdates-passreports.php"  style="color: purple">
+                                <i class="fas fa-copy"></i>Access Log</a>
+                        </li>  
+
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
